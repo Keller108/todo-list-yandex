2298de3eacf61abc5fb0a94e677e4c8529d6e52f
Nikita Isaev <keller108@yandex.ru>
2021-03-05 14:27:41 +0500

    feat: render single task from form

--- js/index.js
+++ js/index.js
@@ -7,23 +7,41 @@ const TODO_LIST = [
 ];
 
 const container = document.querySelector('.todo__list');
+const todoForm = document.querySelector('.todo__form');
+
+
+function createTaskHTMLString(item) {
+    return `
+	<li class="todo__item task">
+	<div class="task__info">
+	  <p class="task__name">${item.title}</p>
+	</div>
+	<div class="task__controls">
+	  <button class="task__btn task__btn_copy" type="button"><img src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
+	  <button class="task__btn task__btn_delete" type="button"><img src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
+	</div>
+  </li>
+	`;
+};
 
 function renderList() {
-    const result = TODO_LIST.map(function(item) {
-        return `
-		<li class="todo__item task">
-		<div class="task__info">
-		  <p class="task__name">${item.title}</p>
-		</div>
-		<div class="task__controls">
-		  <button class="task__btn task__btn_copy" type="button"><img src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
-		  <button class="task__btn task__btn_delete" type="button"><img src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
-		</div>
-	  </li>
-		`;
-
-    }).join('');
+    const result = TODO_LIST.map(createTaskHTMLString).join('');
     container.insertAdjacentHTML('afterbegin', result);
 }
 
+function addTaskForListener() {
+
+}
+
 renderList();
+
+function addTaskForListener(evt) {
+    evt.preventDefault();
+    const input = document.querySelector('.todo__input');
+    const inputTitle = input.value;
+    const newTask = createTaskHTMLString({ title: inputTitle });
+    container.insertAdjacentHTML('afterbegin', newTask);
+    input.value = '';
+}
+
+todoForm.addEventListener('submit', addTaskForListener);
